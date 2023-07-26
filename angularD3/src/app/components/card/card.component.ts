a429ed7e6377b5e95145264357dd74e8b6375204
andreas ubertini <ubertiniandreas@gmail.com>
2023-07-26 14:59:44 +0200

    updated tutto funziona ma necessario aggiornare manualmente

--- angularD3/src/app/components/card/card.component.ts
+++ angularD3/src/app/components/card/card.component.ts
@@ -1,4 +1,6 @@
 import {Component, Input} from '@angular/core';
+import{PostService} from "../../post.service";
+import {Post} from "../../models/post";
 
 @Component({
   selector: 'app-card',
@@ -11,6 +13,10 @@ export class CardComponent {
   @Input() postBody!:string
   @Input() postActive!:boolean
   @Input() postType!:string
-
-
+ constructor(private postSvc:PostService) {
+ }
+  modifyPost(id:number, body:string, title:string, type:string, active:boolean){
+     const data = new Post(id, body, title, type, active)
+    this.postSvc.modifyPost(data, id)
+}
 }
--- angularD3/src/app/post.service.ts
+++ angularD3/src/app/post.service.ts
@@ -12,14 +12,28 @@ export class PostService {
   constructor(){}
 
   fetchPosts(){
-    fetch('../assets/db.json')
+    fetch('http://localhost:3000/db')
     .then(res => res.json())
-    .then(data  => data.forEach((element:Post) => {
+    .then(data  => data['db'].forEach((element:Post) => {
       const newPost = new Post(element.id, element.body, element.title, element.type, element.active)
       this.posts.push(newPost)
     })
     )
     .catch(err => alert(err))
+  }
+
+  modifyPost(data: Post, id: number){
+    fetch(`http://localhost:3000/db/${id}`, {
+      method: "PUT", // or 'PUT'
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }).then(data=>{
+      return data.json()
+    }).then(res=>{
+      console.log(res)
+    })
   }
 
   getPosts():Post[]{
@@ -28,7 +42,7 @@ export class PostService {
   }
   getActivePosts():Post[]{
     this.fetchPosts()
-    this.activePosts = this.posts.filter(el=>el.active===true)
+    this.activePosts = this.posts.filter(el=>el.active)
     console.log(this.activePosts, 'this')
     this.posts = []
     return this.activePosts
